dca49628ef708fd8561ccf46dbd04973d2abfd30
mon1ks <sashaberdar2002@gmail.com>
2022-02-07 19:07:25 +0300

    Adding simple types

---
+++ src/aberdar/initializingTypes/simpleTypes/SimpleTypes.java
@@ -0,0 +1,30 @@
+package aberdar.initializingTypes.simpleTypes;
+
+public class SimpleTypes {
+
+    public static void main(String[] args) {
+
+        // Int
+        final int number = 19;
+
+        // Double
+        double billion = 1E+9;
+        final double exp =  2.7182818;
+
+        // Flout
+        final float pi = 3.14f;
+
+        // String
+        final String firstString = "Hello, world!";
+
+        // Bool
+        final boolean denial = false;
+
+        // Char
+        final char charF = 'F';
+
+        // Tuple (Int, String)
+        // Pair<Integer, String> one = new Pair<Integer, String>(1, "one");
+
+    }
+}
