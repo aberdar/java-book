4bfdedf73af5d8266fec7bcbe03927ddf5b22511
mon1ks <sashaberdar2002@gmail.com>
2022-02-10 10:49:13 +0300

    Adding Declaration And Initialization file

---
+++ src/aberdar/interfaces/declarationAndInitialization/DeclarationAndInitialization.java
@@ -0,0 +1,12 @@
+package aberdar.interfaces.declarationAndInitialization;
+
+public class DeclarationAndInitialization {
+
+    public static void main(String[] args) {
+        // Printable printable = new Printable();
+    }
+}
+
+interface Printable {
+    void Print();
+}
